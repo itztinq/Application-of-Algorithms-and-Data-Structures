1609de04de997a0aa973894e0b782f03c98749ff
Martina <tina.stojanovska7@gmail.com>
2025-06-04 21:03:11 +0200

    .

---
+++ lists/src/lists/NiziListi_12.java
@@ -0,0 +1,21 @@
+package lists;
+import java.util.*;
+
+public class NiziListi_12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        DLL<Integer> dll = new DLL<>();
+        for(int i = 0; i < n; i++) {
+            dll.insertLast(sc.nextInt());
+        }
+        int k = sc.nextInt();
+        System.out.println(dll);
+        for(int i = 0; i < k; i++) {
+            int x = dll.deleteFirst();
+            dll.insertLast(x);
+        }
+        System.out.println(dll);
+    }
+}
